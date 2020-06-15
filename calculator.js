f7b06af8325ad8655be26098be5f0689230064d7
fawaz <st0ock@hotmail.com>
2020-06-15 20:07:58 +0300

    solution

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
 // Define the numbers and the operation here
+let num1 = 3;
+let num2 = 5;
+let addition = (num1+num2)
+
 
 // Write if-statements here
+if (addition==8){console.log("addition");}
+else {
+  console.log("try again");}
