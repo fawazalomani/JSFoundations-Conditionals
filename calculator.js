5d2610879944fa1049fb173507e04e397794087e
fawaz <st0ock@hotmail.com>
2020-06-18 14:23:45 +0300

    new solution after practice

--- calculator.js
+++ calculator.js
@@ -1,10 +1,26 @@
 // Define the numbers and the operation here
-let num1 = 3;
-let num2 = 5;
-let addition = (num1+num2)
+//let num1 = 3;
+//let num2 = 5;
+//let addition = (num1+num2)
 
 
 // Write if-statements here
-if (addition==8){console.log("addition");}
-else {
-  console.log("try again");}
+//if (addition==8){console.log("addition");}
+//else {
+ // console.log("try again");}
+
+
+ // Define the numbers and the operation here
+
+const num1 = 10;
+const num2 = 300;
+let opreation = "/";
+// Write if-statements here
+
+if (opreation === "+") {
+  console.log(` ${num1} + ${num2} = ${num1 +num2} A`);
+} else if  (opreation === "-") {
+  console.log(` ${num1} - ${num2} = ${num1 - num2} S`);
+} else if  (opreation === "*") {console.log(` ${num1} * ${num2} = ${num1 * num2} M`);
+}
+else if  (opreation === "/") console.log(` ${num1} / ${num2} = ${num1 / num2} M`);
